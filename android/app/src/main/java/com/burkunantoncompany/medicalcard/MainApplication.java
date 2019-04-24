e9ef307b69c107e710e5304648531039236ecb45
anton <burkun.anton@gmail.com>
2019-04-24 11:57:12 +0300

    added auth process through Facebook for Android

--- android/app/src/main/java/com/burkunantoncompany/medicalcard/MainApplication.java
+++ android/app/src/main/java/com/burkunantoncompany/medicalcard/MainApplication.java
@@ -12,6 +12,10 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
 
 import io.invertase.firebase.RNFirebasePackage;
 // optional packages - add/remove as appropriate
@@ -36,6 +40,12 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
+   private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+    protected static CallbackManager getCallbackManager() {
+      return mCallbackManager;
+    }
+
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -46,11 +56,12 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
-            new FBSDKPackage(),
+            // new FBSDKPackage(),
             new AsyncStoragePackage(),
             new ImagePickerPackage(),
            // new ImageResizerPackage(),
            // new RNFSPackage(),
+        new FBSDKPackage(mCallbackManager),
         new RNFirebasePackage(),
         // add/remove these packages as appropriate
         new RNFirebaseAdMobPackage(),
@@ -86,5 +97,6 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    AppEventsLogger.activateApp(this);
   }
 }
